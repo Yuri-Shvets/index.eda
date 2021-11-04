b39bd15137e26e722cf183c782afe03fb22666e8
Юрий Швец <92976173+Yuri-Shvets@users.noreply.github.com>
2021-11-04 22:31:51 +0200

    Переделал все

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// "use strict";
+// Слайдер -------------------------------------------
+const swiper = new Swiper('.swiper', {
+  initialSlide: 1,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+// Слайдер -------------------------------------------
+// Табы -------------------------------------------
+let mixer = mixitup('.categories__container');
+// Табы -------------------------------------------
